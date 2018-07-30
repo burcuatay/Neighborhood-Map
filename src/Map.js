e9a4c586eb6323d8058b088ae24ebf13abb886fe
Burcu Atay <batay501@gmail.com>
2018-07-30 13:05:01 +0300

    renders the istanbul map

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react';
 
-function Map(props){
-    const {map, marker} = props;
-    return(
 
-    )
-
-}
+class Map extends Component {
+    static defaultProps = {
+      center: {
+        lat: 41.03,
+        lng: 28.98
+      },
+      zoom: 12
+    };
+  
+    render() {
+      return (
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyB92lneaZL30eo0_Uoglb2mMBeKBYdvRz8' }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+          </GoogleMapReact>
+        </div>
+      );
+    }
+  }
+  
+  export default Map;
