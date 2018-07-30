377fa7f9e5209fee19d430ca0e2b58d6d02ef9f2
Burcu Atay <batay501@gmail.com>
2018-07-30 16:42:19 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NeighborhoodMap from './NeighborhoodMap'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NeighborhoodMap/>
+      </div>
+    );
+  }
+}
+
+export default App;
